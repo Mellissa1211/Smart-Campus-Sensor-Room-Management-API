a89b23f5570ed1ccf3c4d5fecc2a1981c69cda7f
Lissa2005 <mellissamangalaari2@gmail.com>
2026-04-23 13:59:12 +0530

    made changes to the code

--- smart-campus-api/src/main/java/com/smartcampus/api/SmartCampusApplication.java
+++ smart-campus-api/src/main/java/com/smartcampus/api/SmartCampusApplication.java
@@ -1,13 +1,16 @@
 package com.smartcampus.api;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
 /**
  * TASK 1.1: REST API Versioning
  * Establishing the base URI for the API at /api/v1
  */
 @ApplicationPath("/api/v1")
-public class SmartCampusApplication extends Application {
-    // Empty body — the annotation does everything
+public class SmartCampusApplication extends ResourceConfig {
+    public SmartCampusApplication() {
+        // This automatically finds your @Path and @Provider classes
+        packages("com.smartcampus"); 
+    }
 }
